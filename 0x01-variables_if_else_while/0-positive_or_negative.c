cf72a0352e7cb3c997d7ab8d8b6edfb7d430ee8b
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-06 12:32:59 -0700

    0-positive_or_negative.c - 0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,20 +9,20 @@
  * Description: program will assign a random number to the 
  * variable n each time it is executed
  *
- * Return: Always (0) - Success
- *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n > 0)
 		printf("%d is positive\n", n);
-	else 
+	else
 		printf("%d is negative\n", n);
 	return (0);
 }
