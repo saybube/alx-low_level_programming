1842a2baf1d92893fa6f5b60d322bd1adf83a230
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-06 22:59:31 -0700

    7-print_tebahpla.c - program that prints the lowercase alphabet in reverse, followed by a new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alph;
+
+	for (alph = 122; alph > 96, alph--)
+	{
+		putchar(alph);
+	}
+
+	putchar(10);
+	return (0);
+}
