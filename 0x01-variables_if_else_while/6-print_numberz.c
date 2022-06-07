b938b870d891e368033aabd597c9ba77a1f661f8
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-06 22:52:02 -0700

    6-print_numberz.c - prints all single digit numbers of base 10 starting from 0, followed by a new line without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 48; numb <= 57; numb++)
+	{
+		putchar(numb);
+	}
+
+	putchar(10);
+	return (0);
+}
