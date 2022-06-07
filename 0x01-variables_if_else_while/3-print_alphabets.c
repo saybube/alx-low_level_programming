9d0836dc5b78f8c4759175d0a4685dd4e4f8d7d5
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-06 22:29:03 -0700

    3-print_alphabets.c - prints the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in lower then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alph;
+
+	for (alph = 97; alph <= 122; alph++);
+	{
+		putchar(alph);
+	}
+
+	for (alph = 65; alph <= 90; alph++);
+	{
+		putchar(alph);
+	}
+
+	putchar(10);
+
+	return(0);
+}
