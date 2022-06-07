7b31ef64740ad4346d53847de6367b09cd24560a
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-06 21:49:47 -0700

    2-print_alphabet.c - prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: prints the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(1);
+		i++;
+	}
+	putchar(10);
+
+	return(0);
+}
