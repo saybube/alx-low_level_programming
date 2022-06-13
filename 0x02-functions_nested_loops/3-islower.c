eb96c1dee73cd309fd16258f451e5c55a7092a07
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-13 14:37:57 -0700

    3-islower.c - function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _islower - checks for lowercase character
+ *
+ *  Return: 1 if lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		Return (1);
+	}
+	else
+	{
+		Return (0);
+	}
+}
