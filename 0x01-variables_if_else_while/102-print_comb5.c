dd774e1bcd3dde0a8952bdb5eb439336d63f6edd
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-15 14:11:28 -0700

    two two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	a = 48;
+	while (a < 58)
+	{
+		b = 48;
+		while (b < 58)
+		{
+			d = a + 1;
+			c = a;
+			while (c < 58)
+			{
+				while (d < 58)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					if (a < 57 || b < 56 || c < 57 || d < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					d++;
+				}
+				d = 48;
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+	putchar(10);
+	return (0);
+}
