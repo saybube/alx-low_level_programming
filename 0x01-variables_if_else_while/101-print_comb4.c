e8e95fdf0c25d576282f35e22db028f29cc84757
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-15 13:08:37 -0700

    print triple combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+	
+	int a = 48;
+	int b = 48;
+	int c = 48;
+
+	while (a < 58)
+	{
+		b = b + 1;
+		while (b < 58)
+		{
+			c = c +1;
+			while (c < 58)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a < 55 || b < 56 || c < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+	putchar(10);
+	return (0);
+}
