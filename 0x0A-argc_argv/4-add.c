2fa62996279cf2ce4557e83e1f1cb6dd6faba8e6
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-24 15:31:31 -0700

    posse

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
