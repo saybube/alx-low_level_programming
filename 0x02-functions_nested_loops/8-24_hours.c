f70ebbd1bdfac942651bc8d25d607991e1e7b19c
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-06-30 01:11:53 -0700

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include <main.h>
+/** jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hb;
+	int hs;
+	int mb;
+	int ms;
+	int i = 9;
+
+	hs = 0;
+	while (hs <= 2)
+	{
+		if (hs == 2)
+		{
+			i = 3;
+		}
+		hb = 0;
+		while (hb <= a)
+		{
+			ms = 0;
+			while (ms <= 5)
+			{
+				mb =0;
+				while (mb <= 9)
+				{
+					_putchar('0' + hs);
+					_putchar('0' + hb);
+					_putchar(':');
+					_putchar('0' + ms);
+					_putchar('0' + mb);
+					_putchar('\n');
+					mb++;
+				}
+				ms++;
+			}
+			hb++;
+		}
+		hs++
+	}
+}
