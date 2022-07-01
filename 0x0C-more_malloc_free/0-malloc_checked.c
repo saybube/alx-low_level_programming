cb3c373003be188fd15a3ab9ac69bc09f2f506ea
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-07-01 13:25:01 -0700

    allocate mem

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+include "main.h'
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: int size of memory to be allocated
+ * Return: Void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
