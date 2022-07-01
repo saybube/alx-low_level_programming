acb2a31c3aaad079c6bd647684a50f07c7f41dbf
Ebubechukwu Nwamaka <ebubenwamaka@gmail.com>
2022-07-01 14:02:16 -0700

    allocate mem for array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: describes the number of members
+ * @size: describes the number of bytes
+ * Return: pointer to allocated memory and same as the string
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	unsigned int i;
+	void *j;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+
+	j = arr;
+	for (i = 0; i < nmemb * size; i++)
+		j[i] = 0;
+	return (arr);
+}
