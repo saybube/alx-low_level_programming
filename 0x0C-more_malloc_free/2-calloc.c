6bb535fe9dd173456cd186d497b94ddeb11328b0
Ebubechukwu Nwamaka <76922403+saybube@users.noreply.github.com>
2022-07-02 11:34:31 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *arr;
 	unsigned int i;
 	char *j;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
+
 	j = arr;
 	for (i = 0; i < nmemb * size; i++)
 		j[i] = 0;
